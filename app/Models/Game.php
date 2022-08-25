ebcdb1a9bbe83765bf06174f330b5da437eee46c
Simone <cirino.simone99@gmail.com>
2022-08-25 16:14:33 +0200

    Aggiusta relazione uno a molti tra game e user

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     protected $fillable = ['title', 'image', 'price', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User'); // il belongs to sta dalla parte dell'1 della relazione e il nome deve essere singolare
+    }
 }
